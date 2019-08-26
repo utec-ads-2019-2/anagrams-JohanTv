a9a4a663c603f3383babb2b8a38f12554425c5a1
JohanTv <40219652+JohanTv@users.noreply.github.com>
2019-08-26 04:23:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
-
+#include <vector>
+#include <map>
+#include <algorithm>
 using namespace std;
 
-int main() {
-	return EXIT_SUCCESS;
+map<string,int> anagramas;
+vector<string> palabras;
+
+string key(string cadena){
+    for(int i=0;i<cadena.length();i++){
+        cadena[i]=tolower(cadena[i]);
+    }
+    sort(cadena.begin(),cadena.end());
+    return cadena;
 }
 
+void impresion(){
+    string temp;
+    vector<string> save;
+    for(auto it= palabras.begin(); it!=palabras.end(); it++){
+        if(anagramas[key((*it))]==1){
+            save.push_back(*it);
+        }
+    }
+    sort(save.begin(),save.end());
+    for(auto it=save.begin();it!=save.end();it++){
+        cout<<*it<<endl;
+    }
+}
+
+
+int main(){
+    string palabra;
+    while(cin>>palabra){
+        if(palabra=="#") break;
+        else{
+            palabras.push_back(palabra);
+            anagramas[key(palabra)]++;
+        }
+    }
+    impresion();
+
+}
